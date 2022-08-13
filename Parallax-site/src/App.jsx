454493a2d09b6a2b1d202bdaea99840558600755
samuel cordova <cordovas1337@gmail.com>
2022-08-12 19:03:35 -0500

    testing out parallax

--- Parallax-site/src/App.jsx
+++ Parallax-site/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useRef } from 'react'
 import './App.css'
+import moon from './moon.png';
+import land from './land.png';
+import cat from './cat.gif';
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [count, setCount] = useState(0)
+const ref =useRef();
   return (
-    <div className="App">
-      <div>
+    <div>
+
+<Parallax pages={4} ref={ref}>
+     <ParallaxLayer speed={1}>
+            <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.5}>
+            <h2>Web development is fun!</h2>
+        </ParallaxLayer>
+</Parallax>
+    
+    {/* <div className="App"> */}
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
@@ -26,7 +42,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+  {/* </div> */}
     </div>
   )
 }
